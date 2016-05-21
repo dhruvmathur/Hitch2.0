b1a6283a7c2462ead0cc864bfcbc0ec2fb1b2788
Shreyansh Anand <shreyanand@yandex.com>
2016-05-21 16:40:13 -0400

    Initial Commit

---
+++ Hitch2.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hitch2.0
+//
+//  Created by Shreyansh Anand on 2016-05-21.
+//  Copyright © 2016 Shred130 Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
